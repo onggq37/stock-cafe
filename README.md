9d9042a34b19fa4e8e795683140d9b993de6d5b6
onggq37 <84619503+onggq37@users.noreply.github.com>
2021-09-02 08:49:50 +0800

    adding name

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # stock-cafe
 web app to track your stock portfolio
+
+thing i want to add
+- make row in table clickable instead of just the symbol
--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const mongoose = require("mongoose");
 const axios = require('axios').default;
 const transactionModel = require("./models/transaction")
-const stockModel = require("./models/stock")
 const methodOverride = require("method-override");
 
 
@@ -24,13 +23,76 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 
+app.get('/stockCafe/seed', async (req, res) => {
+    const seedTrans =
+    [
+        {
+          symbol: 'TSLA',
+          action: 'buy',
+          date: new date("2021-08-10T00:00:00.000Z"),
+          units: 10,
+          price: 500,
+        },
+        {
+          symbol: 'TSLA',
+          action: 'buy',
+          date: new date("2021-08-10T00:00:00.000Z"),
+          units: 5,
+          price: 250,
+        },
+        {
+          symbol: 'TSLA',
+          action: 'buy',
+          date: new date("2021-08-11T00:00:00.000Z"),
+          units: 1,
+          price: 750,
+        },
+        {
+          symbol: 'TSLA',
+          action: 'sell',
+          date: new date("2021-08-04T00:00:00.000Z"),
+          units: 6,
+          price: 600,
+        }
+      ]
+  
+    try {
+      const seedItems = await transactionModel.create(seedTrans)
+      res.send(seedItems)
+    } catch (err) {
+      res.send(err.message)
+    }
+});
+
 //index
 app.get("/stockCafe", async (req,res) => {
+    
+    const allTrans = await transactionModel.find();
+    console.log(allTrans)
     const symbols = [];
+    const allStocks = await transactionModel.find().distinct('symbol');
+    console.log(allStocks);
+    const overallBuy = await transactionModel.aggregate(
+        [
+            {
+                $match: {
+                    action: "buy"
+                }
+            },
+            {
+                $group: {
+                    _id: "$symbol", 
+                    total: {
+                        $sum: "$units"
+                    }
+                }
+            }
+        ]
+    )
 
     //Getting all the distinct symbol from db
+    
     /*
-    const allStocks = await transactionModel.find().distinct('symbol');
     for(const element of allStocks) {
         const stockAttr = {
             name: element,
@@ -65,6 +127,30 @@ app.get("/stockCafe/new", (req,res) => {
 //create
 app.post("/stockCafe", async (req, res, next) => {
     const symbol = req.body.symbol;
+
+    try{
+        const getSymbolInfo = await axios.get(`https://api.polygon.io/v3/reference/tickers?ticker=${symbol}&active=true&sort=ticker&order=asc&limit=10&apiKey=S47tdjxsU3ApK1ky1qC426NglkL3DS4K`)
+        console.log(getSymbolInfo.data.results);
+        if ( getSymbolInfo.data.results === null) {
+            res.redirect("/stockCafe/new?success=false&symbol=invalid");
+        } else {
+            nameOfStock = getSymbolInfo.data.results.name.split('.');
+            const input = {
+                symbol: req.body.symbol,
+                name: nameOfStock[0],
+                action: req.body.action,
+                date: req.body.date,
+                units: req.body.units,
+                price: req.body.price,
+            }
+            const newTransaction = await transactionModel.create(input);
+            res.redirect("/stockCafe")
+        }
+    } catch (e) {
+        console.log("Error", e)
+    }
+
+    /*
     try{
         const getSymbolInfo = await axios.get(`https://api.polygon.io/v3/reference/tickers?ticker=${symbol}&active=true&sort=ticker&order=asc&limit=10&apiKey=S47tdjxsU3ApK1ky1qC426NglkL3DS4K`)
         if ( getSymbolInfo.data.results === null) {
@@ -154,17 +240,23 @@ app.post("/stockCafe", async (req, res, next) => {
     } catch (e) {
         res.send(e.message);
     }
+    */
 
 
 });
 
-//show
-app.get("/stockCafe/:id", (req,res) => {
-    res.send("show");
+//Show
+app.get("/stockCafe/transactions", async (req,res) => {
+    const transList = await transactionModel.find({}).sort('-date');
+    res.render("transaction.ejs", {
+        transList,
+    } );
 })
 
 //edit
-app.get("/stockCafe/:id/edit", (req,res) => {
+app.get("/stockCafe/:id/edit", async (req,res) => {
+    const singleTrans = await transactionModel.find({ _id: `${req.params.id}` });
+    console.log(singleTrans);
     res.send("edit");
 })
 
